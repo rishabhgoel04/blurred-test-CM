6642a82d1103306a35132d6296c06d88941638c5
Rishabh Goel <rishabhgoel@Rishabhs-MacBook-Air.local>
2022-03-24 15:11:49 +0530

    updated scheduler for method running

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ RUN mkdir /code
 WORKDIR /code
 COPY . /code/
 RUN apt-get update && apt-get install -y python3-opencv
-RUN pip install opencv-python
 RUN pip3 install -r requirements.txt
 EXPOSE 5000
 CMD ["python", "-m", "scheduler"]
--- scheduler.py
+++ scheduler.py
@@ -1,23 +1,20 @@
-from subprocess import call
-
 import time
 import os
 from pytz import utc
-
+from import_modules import *
+from helper import *
+from blurred_test import run
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
-
-def job():
-    print("In job")
-    call(['python', 'blurred_test.py'])
+# def job():
+#     print("In job")
+#     call(['python', 'blurred_test.py'])
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     scheduler.configure(timezone=utc)
-    scheduler.add_job(job, trigger='cron', day_of_week=2, hour=8, minute=20)
-    # scheduler.add_job(job, trigger='cron', week="*")
-    # scheduler.add_job(job,'interval', minutes=10)
+    scheduler.add_job(func=run, trigger='cron', day_of_week=3, hour=9, minute=38)
+    # scheduler.add_job(, trigger='cron', day_of_week=3, hour=7, minute=13)
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
